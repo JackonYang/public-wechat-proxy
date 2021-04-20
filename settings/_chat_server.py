26f7dd94c04c253ed0e7afd77ba24d1309ca2d8d
Jackon Yang <i@jackon.me>
2021-04-20 23:25:30 +0800

    update

--- settings/_chat_server.py
+++ settings/_chat_server.py
@@ -2,11 +2,13 @@
 import os
 
 
-CHAT_SERVER_HOST = os.environ.get('CHAT_SERVER_HOST', '127.0.0.1')
-CHAT_SERVER_PORT = os.environ.get('CHAT_SERVER_PORT', '6061')
+CHAT_SERVER_HOST = os.environ.get('CHAT_SERVER_HOST', 'chat.10fen.jackon.me')
+# CHAT_SERVER_PORT = os.environ.get('CHAT_SERVER_PORT', '6061')
 
-CHAT_SERVER_URL = 'http://%s:%s/%s' % (
+CHAT_SERVER_URL = 'https://%s/%s' % (
     CHAT_SERVER_HOST,
-    CHAT_SERVER_PORT,
+    # CHAT_SERVER_PORT,
     'chat/',
 )
+
+print CHAT_SERVER_URL
